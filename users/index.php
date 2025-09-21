59d7db7dc1646c1dbbcc70d32579de89a6925cb0
wormrise <wormrisee@gmail.com>
2025-09-21 22:53:39 +0300

    S

--- users/index.php
+++ users/index.php
@@ -9,6 +9,15 @@
             "cliente_DateStarted": "04-02-2025",
             "cliente_DateExpired": "04-06-9999",
             "status": 1
+        },
+         {
+            "id": 2,
+            "cliente_NOMBRE": "YT",
+            "cliente_ID": "gg_116152060195741273040",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateStarted": "04-02-2025",
+            "cliente_DateExpired": "04-06-2028",
+            "status": 1
         }
     ]
 }
